fe5d8298ec0ae88ba701abb1b54b7190db5099ae
Nayed SAID ALI <nayed.saidali@gmail.com>
2015-12-07 13:08:14 +0100

    level 5

---
+++ beginner-tower/level-5/player.rb
@@ -0,0 +1,25 @@
+class Player
+  @known_health = nil
+  @current_health = nil
+
+  def play_turn(warrior)
+    @known_health = warrior.health
+    @current_health = warrior.health if @current_health == nil
+
+    if warrior.feel.empty?
+      if @current_health > @known_health
+        warrior.walk!
+      elsif @current_health < 12
+        warrior.rest!
+      else
+        warrior.walk!
+      end
+    elsif warrior.feel.captive?
+      warrior.rescue!        
+    else
+      warrior.attack!
+    end
+
+    @current_health = warrior.health
+  end
+end
